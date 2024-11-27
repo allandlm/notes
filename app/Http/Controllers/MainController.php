0538e7d16782dd5e8cf4220761bf87bcb58ba973
Allan Marques <allanlima00@gmail.com>
2024-11-27 15:10:26 -0300

    Adjust on id encrypt, finished

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -57,7 +57,9 @@ class MainController extends Controller
 
     public function editNote($id){
         $id = Operations::decryptId($id);
-
+        if($id === null){
+            return redirect()->route('home');
+        }
         //load note
         $note = Note::find($id);
 
@@ -93,7 +95,9 @@ class MainController extends Controller
         
         //decrypt note_id
         $id = Operations::decryptId($request->note_id);
-
+        if($id === null){
+            return redirect()->route('home');
+        }
         //load note
         $note = Note::find($id);
 
@@ -109,7 +113,9 @@ class MainController extends Controller
     public function deleteNote($id){
         //check if id is encrypted
         $id = Operations::decryptId($id);
-        
+        if($id === null){
+            return redirect()->route('home');
+        }
         //load note
         $note = Note::find($id);
         
@@ -120,6 +126,9 @@ class MainController extends Controller
     public function deleteNoteConfirm($id){
         //check if id is encrypted
         $id = Operations::decryptId($id);
+        if($id === null){
+            return redirect()->route('home');
+        }
 
         //load note
         $note = Note::find($id);
