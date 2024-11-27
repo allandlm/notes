ab85efc0fd2b900a9ca77fbfbb7bf3db3706cd36
Allan Marques <allanlima00@gmail.com>
2024-11-27 00:35:50 -0300

    Added Operations to edit and delete, with encrypted id

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
     public function index(){
         //load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
 
         //show home view
-        return view('home');
+        return view('home', ['notes'=>$notes]);
     }
     public function newNote(){
         echo "I'm creating a new note";
     }
+
+    public function editNote($id){
+        $id = Operations::decryptId($id);
+    }
+    
+    public function deleteNote($id){
+        $id = Operations::decryptId($id);
+    }
+
 }
